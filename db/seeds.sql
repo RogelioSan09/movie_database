b6c53c3d7ac9c3e43268c11ca4ff357ff2719423
eschindev <118208710+eschindev@users.noreply.github.com>
2023-04-28 13:46:45 -0700

    db setup and start of server.js

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO movies (movie_name)
+VALUES ('Up'),
+('Halloween'),
+('Hurt Locker'),
+('Titanic'),
+('Lord of the Rings: The Fellowship of the Ring (extended edition)');
+
+INSERT INTO reviews (movie_id, review) 
+VALUES (1, "This movie is good, but it made me cry so I don't watch it anymore."),
+(2, "This movie made me shit my pants."),
+(3, "This movie is military propaganda."),
+(4, "This movie is aight. Leonardo is pretty hot though."),
+(5, "LORD OF THE RINGS (extended edition is unnecessary, Peter Jackson needs to chill)");
